f9e3a8ecff3af653eb9723206ca93911fc2617bd
alexeykomov <alexeykofficial@gmail.com>
2019-09-11 22:10:04 +0300

    edit: urls

--- src/index.js
+++ src/index.js
@@ -2,27 +2,6 @@ import { removeOutgoingRequests } from './somemodule.js'
 
 const onLoad = () => {
 
-  VK.UI.button('login-button');
-
-  VK.Auth.login((response) => {
-    if (response.session) {
-      /* Пользователь успешно авторизовался */
-
-      if (response.settings) {
-        /* Выбранные настройки доступа пользователя, если они были запрошены */
-      }
-    } else {
-      /* Пользователь нажал кнопку Отмена в окне авторизации */
-    }
-  });
-
-  VK.Api.call('users.get', {user_ids: 6492, v:"5.73"}, function(r) {
-    if(r.response) {
-      alert('Привет, ' + r.response[0].first_name);
-      removeOutgoingRequests();
-    }
-  });
-
 };
 
-document.addEventListener('load', onLoad, false);
+window.addEventListener('load', onLoad, false);
--- target/output-compiled-ui.js
+++ target/output-compiled-ui.js
@@ -1,9 +1,3 @@
-;(function(){document.addEventListener("load", function() {
-  VK.UI.button("login-button");
-  VK.Auth.login(function() {
-  });
-  VK.Api.call("users.get", {$user_ids$:6492, $v$:"5.73"}, function($r$jscomp$1$$) {
-    $r$jscomp$1$$.response && (alert("Привет, " + $r$jscomp$1$$.response[0].$first_name$), console.log("Starting to remove outgoing requests."));
-  });
+;(function(){window.addEventListener("load", function() {
 }, !1);
 })();
